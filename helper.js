a70d0df6d0e607e0f746fbfb35026abd56f2d607
Philip Langbehn <philip.langbehn@daimler.com>
2017-10-27 08:54:55 +0200

    use JSON.stringify in favor of custom object to string mapping

--- helper.js
+++ helper.js
@@ -5,20 +5,13 @@ const engine = Random.engines.mt19937().autoSeed();
 
 module.exports = {
   hashMe: (message = '') => {
-    // In case message is an object we need to sort it
-    if (typeof message !== 'string') {
-      const sortedObjectArray = [];
-      const messageKeys = Object.keys(message).sort();
+    const sha256 = hash.sha256();
 
-      messageKeys.forEach(key => sortedObjectArray.push(`${key}:${message[key]}`));
+    typeof message === 'string' ?
+      sha256.update(message) :
+      sha256.update(JSON.stringify(message));
 
-      return hash.sha256().update(`{${sortedObjectArray.join(',')}}`).digest('hex');
-    }
-    else {
-      return hash.sha256().update(message).digest('hex');
-    }
-
-
+    return sha256.digest('hex');
   },
   getRandomInteger: (min, max) => {
     return Random.integer(min, max)(engine);
